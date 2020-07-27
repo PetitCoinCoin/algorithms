74645a363aae07c239df1c1eae228910b3405c83
Amarnier <andrea.marnier@gmail.com>
2020-07-27 14:54:02 +0200

    using heap -- time reduced by 10

--- Course2/assignment2.py
+++ Course2/assignment2.py
@@ -4,8 +4,10 @@ warnings.filterwarnings("ignore")
 from collections import defaultdict 
 import pandas as pd
 import time
+import heapq
 
 def dijkstra_naive(graph, start=1, n=200):
+    start_time = time.time()
 
     # Init
     processed = [start]
@@ -22,10 +24,49 @@ def dijkstra_naive(graph, start=1, n=200):
         processed.append(new_vertex)
         distances[new_vertex] = df.loc[idx_min[0],'dijkstra']
     
+    print("--- %s seconds ---" % (time.time() - start_time))
+    return distances
+
+
+def dijkstra_heap(graph, start=1, n=200):
+    start_time = time.time()
+    
+    # Init
+    distances = defaultdict(lambda : 1000000)
+    distances[start] = 0
+
+    remaining = []
+    df = graph[graph['tail'] == start]
+    for i in range(1, n+1):
+        if i != start:
+            idx = df[df['head'] == i].index
+            try:
+                dijkstra = df.loc[idx[0], 'weight']
+                distances[df.loc[idx[0], 'head']] = dijkstra
+            except IndexError:
+                pass
+            heapq.heappush(remaining, (distances[i], i))
+        
+
+    # Main
+    while len(remaining) > 0:
+        new_vertex = heapq.heappop(remaining)
+        distances[new_vertex[1]] = new_vertex[0]
+        # Update keys
+        df = graph[graph['tail'] == new_vertex[1]]
+        for idx in df.index:
+            h = df.loc[idx, 'head']
+            if (distances[h], h) in remaining:
+                remaining.remove((distances[h], h))
+                w = df.loc[idx, 'weight']
+                distances[h] = min(distances[h], distances[new_vertex[1]] + w)
+                heapq.heappush(remaining, (distances[h], h))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
     return distances
 
 if __name__ == '__main__':
-    start_time = time.time()
+    
     # Get clean input data
     with open('dijkstraData.txt', 'r') as f:
         data = f.readlines()
@@ -41,9 +82,13 @@ if __name__ == '__main__':
             weights.append(v[1])
     graph_df = pd.DataFrame({'tail': tails, 'head': heads, 'weight': weights})
 
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print("__________Naïve approach___________")
     dist = dijkstra_naive(graph_df)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    result = [dist[x] for x in [7,37,59,82,99,115,133,165,188,197]]
+    print(result)
+
+    print("__________Using heap___________")
+    dist = dijkstra_heap(graph_df)
     result = [dist[x] for x in [7,37,59,82,99,115,133,165,188,197]]
     print(result)
     
